45663fa3f5353f9e004ef149cb9e8fed2c7ad5fe
Eedrah <eedrah@users.noreply.github.com>
2016-11-13 09:00:23 -0600

    fix paths

--- music/download-song.sh
+++ music/download-song.sh
@@ -1,2 +1,2 @@
 # Is useful: cat | ./download-song.sh
-../../../apps/youtube-dl.exe --ffmpeg-location ../../../apps/ffmpeg-20160828-a37e6dd-win64-static/bin/ffmpeg.exe  --format bestaudio --audio-format mp3 --extract-audio --output '%(title)s.%(ext)s' --default-search ytsearch --batch-file -
+${BASH_SOURCE%/*}/../../../apps/youtube-dl.exe --ffmpeg-location ${BASH_SOURCE%/*}/../../../apps/ffmpeg-20160828-a37e6dd-win64-static/bin/ffmpeg.exe  --format bestaudio --audio-format mp3 --extract-audio --output '%(title)s.%(ext)s' --default-search ytsearch --batch-file -
