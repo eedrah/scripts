15c2774ef58247c015031d173d84e149b0c8c1e3
Eedrah <eedrah@users.noreply.github.com>
2016-11-13 07:59:30 -0600

    add script - download song

---
+++ music/download-song.sh
@@ -0,0 +1,1 @@
+../../../apps/youtube-dl.exe --ffmpeg-location ../../../apps/ffmpeg-20160828-a37e6dd-win64-static/bin/ffmpeg.exe  --format bestaudio --audio-format mp3 --extract-audio --output '%(title)s.%(ext)s' --default-search ytsearch --batch-file -
