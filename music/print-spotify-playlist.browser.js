c4f22c80ed8c97cd4c51b1d3394e56d9c031ae75
Eedrah <eedrah@users.noreply.github.com>
2016-11-13 10:41:51 -0600

    include playlist name

--- music/print-spotify-playlist.browser.js
+++ music/print-spotify-playlist.browser.js
@@ -1,8 +1,11 @@
-tidyString = s => s.trim().replace(/\s+/g, ' ').replace(':', '')
-artists = [].map.call(document.querySelectorAll('td.tl-artists'), n => n.textContent).map(tidyString)
-titles = [].map.call(document.querySelectorAll('td.tl-name'), n => n.textContent).map(tidyString)
-tracks = []
-for (i = 0; i < titles.length; i++) {
-  tracks[i] = artists[i] + ' ' + titles[i]
+function getTracks () {
+  var tidyString = s => s.trim().replace(/\s+/g, ' ').replace(':', '')
+  var playlistName = tidyString(document.querySelector('h1.main').textContent)
+  var artists = [].map.call(document.querySelectorAll('td.tl-artists'), n => n.textContent).map(tidyString)
+  var titles = [].map.call(document.querySelectorAll('td.tl-name'), n => n.textContent).map(tidyString)
+  var tracks = []
+  for (var i = 0; i < titles.length; i++) {
+    tracks[i] = artists[i] + ' ' + titles[i]
+  }
+  prompt(null, playlistName + '\n' + tracks.join('\n'))
 }
-console.log(tracks.join('\n'))
