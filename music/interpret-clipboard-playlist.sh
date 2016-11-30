85bfd1732bc3cff9162ca4e85a707edd4a34a6eb
eedrah <eedrah@users.noreply.github.com>
2016-11-30 02:48:23 -0600

    Update interpret-clipboard-playlist.sh

--- music/interpret-clipboard-playlist.sh
+++ music/interpret-clipboard-playlist.sh
@@ -1,5 +1,20 @@
-folder=`head -1 /dev/clipboard`
-mkdir "$folder"
-cd "$folder"
-tail -n +2 /dev/clipboard > ref.txt
-cd ..
+cat /dev/clipboard | {
+  is_in_playlist=false
+  while IFS= read -r ROWW; do
+    ROW="$(printf '%s' "$ROWW" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+    if [ "$is_in_playlist" = false ]; then 
+      if [[ -n "$ROW" ]]; then
+        mkdir "$ROW"
+        cd "$ROW"
+        is_in_playlist=true
+      fi
+    else 
+      if [[ -n "$ROW" ]]; then
+        printf '%s\n' "$ROW" >> ref.txt
+      else
+        is_in_playlist=false
+        cd ..
+      fi
+    fi
+  done
+}
