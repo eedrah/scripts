c2a46e57114d89a858bb420b4369426038b2d1bf
Eedrah <eedrah@users.noreply.github.com>
2018-07-08 00:19:07 -0500

    update list of bad processes

--- kill-bad-processes.bat
+++ kill-bad-processes.bat
@@ -1,3 +1,4 @@
+
 @echo off
 taskkill /F ^
  /im scrybe.exe ^
@@ -14,7 +15,7 @@ taskkill /F ^
  /im Osa.exe ^
  /im soffice.exe ^
  /im AdobeARM.exe ^
- /im AAM^ Update^ Notifier.exe ^
+ /im "AAM Update Notifier.exe" ^
  /im Jusched.exe ^
  /im DivXUpdate.exe ^
  /im NeroCheck.exe ^
@@ -31,9 +32,30 @@ taskkill /F ^
  /im OSPPSVC.exe ^
  /im Sidebar.exe ^
  /im wmpnetwk.exe ^
+ /im armsvc.exe ^
+ /im audiodg.exe ^
+ /im TBNotifier.exe ^
+ /im mDNSResponder.exe ^
+ /im CCleaner64.exe ^
+ /im livecomm.exe ^
+ /im RAVBg64.exe ^
+ /im HPMSGSVC.exe ^
+ /im hpqwmiex.exe ^
+ /im HPSupportSolutionsFrameworkService.exe ^
+ /im HPWMISVC.exe ^
+ /im SearchIndexer.exe ^
+ /im HeciServer.exe ^
+ /im RtkAudioService64.exe ^
+ /im BTDevMgr.exe ^
+ /im TWCU.exe ^
+ ^
  /im ica.exe ^
+ /im apnmcp.exe ^
   
  
 :loop
-taskkill /F /im ica.exe
+taskkill /F ^
+ /im ica.exe ^
+ /im apnmcp.exe ^
+  
 goto loop
