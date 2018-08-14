4e1a4049f8c39ada238957fd7f7d903107ec7405
Eedrah <eedrah@users.noreply.github.com>
2018-08-14 15:13:52 -0500

    add jitter

--- whatsapp/functions.js
+++ whatsapp/functions.js
@@ -15,12 +15,26 @@ function sendMessage(message) {
 }
 
 function sendMessageWithInterval(message, seconds) {
+  const jitter = 0.2
   sendMessage(message)
-  setInterval(() => sendMessage(message), seconds * 1000)
+  setInterval(
+    () => sendMessage(message),
+    seconds * 1000 * (1 + jitter - 2 * jitter * Math.random())
+  )
 }
 
 function sendMessagesWithInterval(arrayOfMessages, seconds) {
+  const jitter = 0.2
   arrayOfMessages.forEach((message, index) => {
-    setTimeout(() => sendMessage(message), index * seconds * 1000)
+    setTimeout(
+      () => sendMessage(message),
+      index * seconds * 1000 * (1 + jitter - 2 * jitter * Math.random())
+    )
   })
 }
+
+//function sendMessageTo(person, message) {
+//  const currentPerson = document.querySelector('header span[title]').title
+//  const htmlPersonToSendTo = document.querySelector(`span[title="${person}"]`)
+//  document.querySelector('input[title="Search or start new chat"]')
+//}
